63f88db80abc466488cb48511d8a0e4570625b5b
Noriyo Akita <noriyo.akita@gmail.com>
2016-04-01 07:49:33 +0000

    Generate PasswordResetsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'password_resets/create'
+
+  get 'password_resets/edit'
+
+  get 'password_resets/update'
+
   root :to => 'users#index'
   resources :user_sessions
   resources :users do
