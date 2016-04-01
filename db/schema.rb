b69ed6749f928c8f70e1800cf1f6ad5f1491e4f4
Noriyo Akita <noriyo.akita@gmail.com>
2016-04-01 07:30:31 +0000

    Install reset_password submodule

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401013541) do
+ActiveRecord::Schema.define(version: 20160401072747) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                        null: false
-    t.string   "username",                     null: false
+    t.string   "email",                           null: false
+    t.string   "username",                        null: false
     t.string   "crypted_password"
     t.string   "salt"
     t.datetime "created_at"
@@ -25,11 +25,15 @@ ActiveRecord::Schema.define(version: 20160401013541) do
     t.string   "activation_state"
     t.string   "activation_token"
     t.datetime "activation_token_expires_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
   end
 
   add_index "users", ["activation_token"], name: "index_users_on_activation_token"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token"
   add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
