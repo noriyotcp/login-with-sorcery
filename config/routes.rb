4a0b0c28cd75ed9928caa4b33d557bd61900c8bc
Noriyo Akita <noriyo.akita@gmail.com>
2016-04-01 08:02:37 +0000

    Implement PasswordResetsController and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/create'
-
-  get 'password_resets/edit'
-
-  get 'password_resets/update'
-
   root :to => 'users#index'
   resources :user_sessions
   resources :users do
@@ -12,7 +6,7 @@ Rails.application.routes.draw do
       get 'activate'
     end
   end
-  resources :password_resets
+  resources :password_resets, only: [:create, :edit, :update]
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
