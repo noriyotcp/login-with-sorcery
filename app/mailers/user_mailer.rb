4434c7548b737da30129b3d21267cbfeb402b1d5
Noriyo Akita <noriyo.akita@gmail.com>
2016-04-01 01:59:13 +0000

    Modify actions on user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,9 +6,10 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.activation_needed_email.subject
   #
   def activation_needed_email
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @user = user
+    @url  = "http://0.0.0.0:3000/users/#{user.activation_token}/activate"
+    mail(:to => user.email,
+         :subject => "Welcome to My Awesome Site")
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,8 +18,9 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.activation_success_email.subject
   #
   def activation_success_email
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @user = user
+    @url  = "http://0.0.0.0:3000/login"
+    mail(:to => user.email,
+         :subject => "Your account is now activated")
   end
 end
